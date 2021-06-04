4f325f8a4a1fead6ece975857a49c4608fa85ddc
Tam Nguyen-Nhat <nguyennhattambk@gmail.com>
2021-06-05 04:39:20 +0700

    ban chinh sua do thi 1

---
+++ edit_ax.m
@@ -0,0 +1,38 @@
+classdef edit_ax < handle
+    properties
+        xlim=[];
+        ylim=[];
+        zlim=[];
+        data=[];
+        xdata=[];
+        ydata=[];
+        zdata=[];
+    end
+    methods
+        function self = edit_ax(gca)
+            self.xlim=get(gca,'XLim');
+            self.ylim=get(gca,'YLim');
+            self.zlim=get(gca,'ZLim');
+            self.data=get(gca,'Children');
+            self.xdata=get(self.data,'XData');
+            self.ydata=get(self.data,'YData');
+            self.zdata=get(self.data,'ZData');
+            
+        end
+        function ex_to_bound(self)
+            set(gca,'XLim',[min(min(self.xdata)) max(max(self.xdata))])
+            set(gca,'YLim',[min(min(self.ydata)) max(max(self.ydata))])
+            set(gca,'ZLim',[min(min(self.zdata)) max(max(self.zdata))])          
+        end
+        function export_pic(s,dirsave,exten)
+                        %apply style sheet info
+            snam='mystyle'; % The name of your style file (NO extension)
+            s=hgexport('readstyle',snam);
+            fnam=fullfile(dirsave,"myfig."+exten); % your file name
+            s.Format = exten; %I needed this to make it work but maybe you wont.
+            set(gcf,'Color','none');
+            hgexport(gcf,fnam,s);
+        end
+    end
+end
+            
